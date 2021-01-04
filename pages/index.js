b335d3f42c520e93093234a391dea7059ce13fd1
Greg Rickaby <gregrickaby@gmail.com>
2021-01-04 09:31:59 -0600

    add <NextSeo /> component and set defaults

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
 import Layout from '@/components/Layout'
 import config from '@/functions/config'
+import {NextSeo} from 'next-seo'
 
 export default function Homepage() {
   return (
-    <Layout
-      title={`${config.siteName} - Homepage`}
-      description="This powers the SEO meta description"
-    >
+    <Layout>
+      <NextSeo
+        title={`${config?.siteName} - Homepage`}
+        description={config?.siteDescription}
+      />
       <h1 className="text-xl">Homepage</h1>
+      <p>
+        Decaffeinated instant froth mug rich aroma mountain lungo robusta
+        ristretto single aromatic milk irish. Half affogato that skinny
+        americano grinder redeye luwak sugar. Viennese arabica dripper sit aroma
+        extra body turkish ristretto. Percolator café panna latte body mug
+        decaffeinated robust est spoon.{' '}
+      </p>
+      <p>
+        Cinnamon grinder frappuccino blue spice saucer bar sweet. Press that con
+        trade spice extraction galão est spice id. Roast cortado milk trifecta
+        aftertaste breve and cream so. Irish shop mocha id filter trifecta
+        variety irish extraction americano macchiato mazagran froth. Breve luwak
+        filter au iced et acerbic carajillo acerbic as robusta as extra redeye
+        irish. Doppio wings frappuccino roast sugar beans go acerbic that
+        arabica.
+      </p>
     </Layout>
   )
 }
