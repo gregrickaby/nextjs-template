011d57137a276ffffa32166dccbea07c5c6d7d9d
Greg Rickaby <gregrickaby@gmail.com>
2021-01-04 09:31:41 -0600

    add <DefaultSeo /> component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,32 @@
+import config from '@/functions/config'
 import '@/styles/index.css'
 import PropTypes from 'prop-types'
+import {DefaultSeo} from 'next-seo'
 
 export default function App({Component, pageProps}) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title={`${config?.siteName} - ${config?.siteDescription}`}
+        description={config?.siteDescription}
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: config?.siteUrl,
+          site_name: `${config?.siteName} - ${config?.siteDescription}`,
+          images: [
+            {
+              url: config?.ogImage,
+              width: 1200,
+              height: 630,
+              alt: config?.siteName
+            }
+          ]
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 App.propTypes = {
